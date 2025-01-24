b817d7fea33987355bf4a309ddb57b6146595ac5
YWChen1169 <yw_chen@asus.com>
2025-01-24 13:59:01 +0800

    hw02_02

--- student_assignment.py
+++ student_assignment.py
@@ -20,13 +20,15 @@ def hw02_1(q1_pdf):
 def hw02_2(q2_pdf):
     loader = PyPDFLoader("./"+q2_pdf)
     documents = loader.load()
-    full_text = "\n".join([doc.page_content for doc in documents])
-    full_document = Document(page_content=full_text.replace(" ", ""))
+    full_text = "".join([doc.page_content for doc in documents])
+    # full_document = Document(page_content=full_text.replace(" ", ""))
     separators=[
-        "\n第"
+        r"第\s+[一二三四五六七八九十百千萬零]+\s+[章]",
+        r"第\s+[0-9]+\s+[條]",
+        r"第\s+[0-9]+-+[0-9]+\s+[條]",
     ]
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 2, chunk_overlap = 0, separators = separators)
-    texts = text_splitter.split_documents([full_document])
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 2, chunk_overlap = 0, separators = separators, is_separator_regex = True)
+    texts = text_splitter.split_text(full_text)
     return len(texts)
 
-# hw02_2(q2_pdf)
+print(hw02_2(q2_pdf))
