bfa2667a7d2741d9e69b89e1c831da220019b699
YWChen1169 <yw_chen@asus.com>
2025-01-24 10:00:33 +0800

    hw02_02

--- student_assignment.py
+++ student_assignment.py
@@ -19,8 +19,9 @@ def hw02_1(q1_pdf):
 def hw02_2(q2_pdf):
     loader = PyPDFLoader("./"+q2_pdf)
     documents = loader.load()
-    separators = ["\n第","\n第"]
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 2000, chunk_overlap = 0, separators = separators)
+    separators = ["\n第","\n   第"]
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 20, chunk_overlap = 0, separators = separators)
     texts = text_splitter.split_documents(documents)
+    return len(texts)
 
 # hw02_2(q2_pdf)
