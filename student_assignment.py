dfa38352f7e538012a1f50a55dfb990263282b5c
YWChen1169 <yw_chen@asus.com>
2025-01-24 10:15:19 +0800

    hw02_02

--- student_assignment.py
+++ student_assignment.py
@@ -1,6 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
+from langchain.docstore.document import Document
 
 q1_pdf = "OpenSourceLicenses.pdf"
 q2_pdf = "勞動基準法.pdf"
@@ -19,9 +20,13 @@ def hw02_1(q1_pdf):
 def hw02_2(q2_pdf):
     loader = PyPDFLoader("./"+q2_pdf)
     documents = loader.load()
-    separators = ["\n第","\n   第"]
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 20, chunk_overlap = 0, separators = separators)
-    texts = text_splitter.split_documents(documents)
+    full_text = "\n".join([doc.page_content for doc in documents])
+    full_document = Document(page_content=full_text.replace(" ", ""))
+    separators=[
+        "\n第"
+    ]
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 30, chunk_overlap = 0, separators = separators)
+    texts = text_splitter.split_documents([full_document])
     return len(texts)
 
 # hw02_2(q2_pdf)
